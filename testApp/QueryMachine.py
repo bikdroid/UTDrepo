6c2688e9317ed177190f1807af94946fa44be451
Bikramjit <edu.bmandal@gmail.com>
2016-10-02 11:07:49 -0500

    commit QueryMachine.py which implements the NLTK

--- testApp/QueryMachine.py
+++ testApp/QueryMachine.py
@@ -4,114 +4,124 @@ from textblob import TextBlob
 
 class QueryDeployer(object):
 
-	def parse(self,text,type) :
+    def parse(self,text,type) :
     
-	    wiki = TextBlob(text)
-		##    wiki = TextBlob("how much balance do I have right now?")
-		##    wiki = TextBlob("Please check balance in my account.")
-		##    wiki = TextBlob("Please check myaccount balance.")
+        wiki = TextBlob(text)
+        ##    wiki = TextBlob("how much balance do I have right now?")
+        ##    wiki = TextBlob("Please check balance in my account.")
+        ##    wiki = TextBlob("Please check myaccount balance.")
 
-	    print(wiki.tags)
+        print(wiki.tags)
 
-	    verbs = list()
-	    nouns = list()
-	    digits = list()
-	    
-	    for word,tag in wiki.tags:
-	        if tag == 'VB':
-	            verbs.append(word.lemmatize())
-	        if tag == 'NN':
-	            nouns.append(word.lemmatize())
-	        if tag == 'CD':
-	            digits.append(word.lemmatize())
+        verbs = list()
+        nouns = list()
+        digits = list()
+        
+        for word,tag in wiki.tags:
+            if tag == 'VB':
+                verbs.append(word.lemmatize())
+            if tag == 'NN':
+                nouns.append(word.lemmatize())
+            if tag == 'CD':
+                digits.append(word.lemmatize())
 
-	    print("verbs are :",verbs)
-	    print("nouns are : " ,nouns)
-	    print("digits are : ", digits)
+        print("verbs are :",verbs)
+        print("nouns are : " ,nouns)
+        print("digits are : ", digits)
 
-	    if type == 2 :
-	        if "89df" in digits:
-	            return "match"
-	        else:
-	            return "not match"
-	        
-	    
-	    if 'check' in verbs:
-	        if 'account balance' in nouns:
-	            return "account check";
-	        if 'balance' in nouns:
-	            return "account check";
+        if type == 2 :
+            if "89df" in digits:
+                return "match"
+            else:
+                return "not match"
+            
+        if 'check' in verbs:
+            if 'account balance' in nouns:
+                return "account check";
+            if 'balance' in nouns:
+                return "account check";
 
-	    if 'how' in wiki.words and 'balance' in nouns:
-	        return "account check";
-	        
-	    if 'account' in nouns and 'money' in nouns and 'how' in wiki.words:
-	        return "account check";
+        if 'tell' in verbs:
+            if 'account balance' in nouns:
+                return "account check";
+            if 'balance' in nouns:
+                return "account check";
 
-	    if 'transaction' in nouns and 'what' in wiki.words:
-	        return "transaction check";
-	    return "Error";
+        if 'cheque' in wiki.words :
+            return "cheque request";
+                
+        if 'how' in wiki.words and 'balance' in nouns:
+            return "account check";
+            
+        if 'account' in nouns and 'money' in nouns and 'how' in wiki.words:
+            return "account check";
 
-	def consumeGETRequestSync(self):
-		data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-		url = 'http://api.reimaginebanking.com/accounts/57f01c50267ebde464c489df/purchases?key=79b678ef3c6bd431c43a335a2b19de15'
-		headers = {"Accept": "application/json"}
-		# call get service with headers and params
-		response = requests.get(url,data = data)
-		## print ("code:",str(response.status_code))
-		## print ("******************")
-		## print ("headers:", str(response.headers))
-		## print ("******************")
-		response_data = str(response.text);
-		#print ("content:", response_data)
-		json_data = json.loads(response_data)
-		print (self.getMerchantName(json_data[0]["merchant_id"]))
-		print (json_data[0]["description"])
-		print (json_data[0]["amount"])
-		return "Your last transaction was : " + "Merchant - " + self.getMerchantName(json_data[0]["merchant_id"]) + " Description - " + json_data[0]["description"] + " Amount - " + str(json_data[0]["amount"]);
+        if 'transaction' in nouns and 'what' in wiki.words:
+            return "transaction check";
+        return "Error";
 
-	def accountCheck(self):
-		data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-		url = 'http://api.reimaginebanking.com/customers/57f019d9267ebde464c489dd/accounts?key=79b678ef3c6bd431c43a335a2b19de15'
-		headers = {"Accept": "application/json"}
-		# call get service with headers and params
-		response = requests.get(url,data = data)
-		## print ("code:",str(response.status_code))
-		## print ("******************")
-		## print ("headers:", str(response.headers))
-		## print ("******************")
-		response_data = str(response.text);
+    def consumeGETRequestSync(self):
+        data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+        url = 'http://api.reimaginebanking.com/accounts/57f01c50267ebde464c489df/purchases?key=79b678ef3c6bd431c43a335a2b19de15'
+        headers = {"Accept": "application/json"}
+        # call get service with headers and params
+        response = requests.get(url,data = data)
+        ## print ("code:",str(response.status_code))
+        ## print ("******************")
+        ## print ("headers:", str(response.headers))
+        ## print ("******************")
+        response_data = str(response.text);
+        #print ("content:", response_data)
+        json_data = json.loads(response_data)
+        print (self.getMerchantName(json_data[0]["merchant_id"]))
+        print (json_data[0]["description"])
+        print (json_data[0]["amount"])
+        return "Your last transaction was : " + "Merchant - " + self.getMerchantName(json_data[0]["merchant_id"]) + " Description - " + json_data[0]["description"] + " Amount - " + str(json_data[0]["amount"]);
 
-		json_data = json.loads(response_data)
-		return " You have " + str(json_data[0]["balance"]) + " amount in your account";
+    def accountCheck(self):
+        data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+        url = 'http://api.reimaginebanking.com/customers/57f019d9267ebde464c489dd/accounts?key=79b678ef3c6bd431c43a335a2b19de15'
+        headers = {"Accept": "application/json"}
+        # call get service with headers and params
+        response = requests.get(url,data = data)
+        ## print ("code:",str(response.status_code))
+        ## print ("******************")
+        ## print ("headers:", str(response.headers))
+        ## print ("******************")
+        response_data = str(response.text);
+
+        json_data = json.loads(response_data)
+        return " You have " + str(json_data[0]["balance"]) + " amount in your account";
     
 
-	def getMerchantName(self,id) :
-		data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
-		url = 'http://api.reimaginebanking.com/merchants/'+ id +'?key=79b678ef3c6bd431c43a335a2b19de15'
-		headers = {"Accept": "application/json"}
-		# call get service with headers and params
-		response = requests.get(url,data = data)
-		## print ("code:",str(response.status_code))
-		## print ("******************")
-		## print ("headers:", str(response.headers))
-		## print ("******************")
-		response_data = str(response.text);
-		json_data = json.loads(response_data)
-		print json_data["name"] 
-		return json_data["name"];
+    def getMerchantName(self,id) :
+        data = '{"query":{"bool":{"must":[{"text":{"record.document":"SOME_JOURNAL"}},{"text":{"record.articleTitle":"farmers"}}],"must_not":[],"should":[]}},"from":0,"size":50,"sort":[],"facets":{}}'
+        url = 'http://api.reimaginebanking.com/merchants/'+ id +'?key=79b678ef3c6bd431c43a335a2b19de15'
+        headers = {"Accept": "application/json"}
+        # call get service with headers and params
+        response = requests.get(url,data = data)
+        ## print ("code:",str(response.status_code))
+        ## print ("******************")
+        ## print ("headers:", str(response.headers))
+        ## print ("******************")
+        response_data = str(response.text);
+        json_data = json.loads(response_data)
+        print (json_data["name"]) 
+        return json_data["name"];
 
-	def response1(self,text,type):
-		print text
-		ret = self.parse(text,type)
-		print ret
-		if ret == "transaction check" :
-			return self.consumeGETRequestSync()
-		if ret == "account check" :
-			return self.accountCheck()
-		if ret == "match":
-			return "You have been authorized. How may I help you ?"
-		if ret == "not match":
-			return "Your provided answer is wrong, please refresh page and try again.."
-		if ret == "Error":
-			return "I am sorry, I might not have an answer, please call a representative at (000)000-0000"
+    def response1(self,text,type):
+        print (text)
+        ret = self.parse(text,type)
+        print (ret)
+        if ret == "transaction check" :
+            return self.consumeGETRequestSync()
+        if ret == "account check" :
+            return self.accountCheck()
+        if ret == "match":
+            return "You have been authorized. How may I help you ?"
+        if ret == "not match":
+            return "Your provided answer is wrong, please refresh page and try again.."
+        if ret == "Error":
+            return "I am sorry, I might not have an answer, please call a representative at (000)000-0000"
+        if ret == "check request" :
+            return "Your request has been forwarded to our team and you will get your cheque on your address in x business days."
