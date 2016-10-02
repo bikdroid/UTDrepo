686dc29ec7594166980267329fa57442f5ba7703
Bikramjit <edu.bmandal@gmail.com>
2016-10-02 04:22:25 -0500

    commitss

--- testApp/static/testApp/js/chatbox-js.js
+++ testApp/static/testApp/js/chatbox-js.js
@@ -1,6 +1,24 @@
 /*Chatbox Javascript*/
 $(document).ready(function(){
 
+
+	 var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                console.log('appended');
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
 	sendMessage = function(text){
 
 	}
@@ -8,7 +26,16 @@ $(document).ready(function(){
 
 		var $message_input, $messages, message;
 		$message_input = $('.message_input');
+		if($message_input.trim()===''){
+			console.log('Seems All Emptys');
+		}
 		$messages = $('.messages');
+		message_side = message_side === 'left'?'right':'left';
+
+		message = new Message({
+			text: text,
+			message_side: message_side
+		});
 		$('.messages').append($message_input.val() + "\n");
 		console.log($message_input.val());
 		console.log("button clicked !!");
