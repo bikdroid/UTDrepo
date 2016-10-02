2b6dd9397b5154933a36e105576fc77790c35dd2
Bikramjit <edu.bmandal@gmail.com>
2016-10-02 03:51:48 -0500

    commitss

--- testApp/static/testApp/js/chatbox-js.js
+++ testApp/static/testApp/js/chatbox-js.js
@@ -1,12 +1,15 @@
 /*Chatbox Javascript*/
 $(document).ready(function(){
 
+	sendMessage = function(text){
+
+	}
 	$(".send_message").click(function(){
 
 		var $message_input, $messages, message;
 		$message_input = $('.message_input');
 		$messages = $('.messages');
-		$('.messages').append($message_input.val());
+		$('.messages').append($message_input.val() + "\n");
 		console.log($message_input.val());
 		console.log("button clicked !!");
 
